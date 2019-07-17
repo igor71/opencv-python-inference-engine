aa02b409127fd3af33d2a7524adf3685112863af
igor71 <34439207+igor71@users.noreply.github.com>
2019-07-17 09:31:09 +0300

    Add files via upload

---
+++ build/download-extract-dir.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd /tmp
+
+git clone https://github.com/banderlog/opencv-python-inference-engine
+
+cd opencv-python-inference-engine/
+
+wget -P $PWD/dldt wget https://github.com/opencv/dldt/archive/2019_R1.1.tar.gz
+
+pv dldt/2019_R1.1.tar.gz | tar xpzf - -C $PWD/dldt
+
+shopt -s dotglob # Includes filenames beginning with a dot.
+
+mv -- dldt/dldt-2019_R1.1/* dldt/
+
+rm -rf dldt/dldt-2019_R1.1 && rm -f dldt/2019_R1.1.tar.gz
+
+wget -P $PWD/opencv https://github.com/opencv/opencv/archive/4.1.0.tar.gz 
+
+pv opencv/4.1.0.tar.gz  | tar xpzf - -C $PWD/opencv
+
+mv -- opencv/opencv-4.1.0/* opencv/
+
+rm -f opencv/4.1.0.tar.gz && rm -rf opencv/opencv-4.1.0
+
+wget -P $PWD/ffmpeg https://github.com/FFmpeg/FFmpeg/archive/n4.1.3.tar.gz 
+
+pv ffmpeg/n4.1.3.tar.gz   | tar xpzf - -C $PWD/ffmpeg
+
+mv -- ffmpeg/FFmpeg-n4.1.3/* ffmpeg/
+
+rm -rf ffmpeg/FFmpeg-n4.1.3 && rm -f ffmpeg/n4.1.3.tar.gz
+
+shopt -u dotglob # Disables previously enabled dotglob option.
+
+echo "All Packages Downloaded and Unpacked Successfully!!!"
---
+++ build/opencv-build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cd /tmp/opencv-python-inference-engine/build/opencv
+
+###### Change export PKG_CONFIG_PATH=$ABS_PORTION/build/ffmpeg/binaries/lib/pkgconfig:$PKG_CONFIG_PATH
+###### -->> You will need to add absolute paths to .pc files
+
+sed -i '11d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '11 a export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/:/usr/share/pkgconfig/:$PKG_CONFIG_PATH' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+###### Change string -D WITH_GTK=OFF \ to -D WITH_GTK=ON \ ##############################
+
+##### Delete Original line ####################################################
+
+sed -i '48d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+##### Adding Modified Line ##########################################################################
+
+sed -i '47 a \    -D WITH_GTK=ON \\' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+############################ Configuring to work with local python (ver.3.6.8) ######################
+
+sed -i '15d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '14 a \# grep "6" from "Python 3.6.8"' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '16d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '15 a \PY_VER=`/usr/local/bin/python --version | sed -rn "s/Python .\\.(.)\\..$/\\1/p"`' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '17d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '16 a \PY_LIB_PATH=`find /usr/lib/x86_64-linux-gnu -iname libpython3.${PY_VER}m.so`' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '23d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '22 a \    -D PYTHON3_EXECUTABLE=`which python3.6` \\' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '25d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '24 a \    -D PYTHON3_NUMPY_INCLUDE_DIRS:PATH=/usr/local/lib/python3.${PY_VER}/dist-packages/numpy/core/include \\' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '26d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '25 a \    -D PYTHON_DEFAULT_EXECUTABLE=`which python3.6` \\' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '27d' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+sed -i '26 a \    -D PYTHON3_PACKAGES_PATH=/usr/local/lib/python3.${PY_VER}/dist-packages \\' /tmp/opencv-python-inference-engine/build/opencv/opencv_setup.sh
+
+ABS_PORTION=/tmp/opencv-python-inference-engine ./opencv_setup.sh
+
+make --jobs=$(nproc --all)
